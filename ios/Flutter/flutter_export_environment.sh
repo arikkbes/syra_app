5da05108bac78a9f9dcdde2b98a1926e4f43e591
besirarikk <arikkbesir@gmail.com>
2026-01-24 02:01:47 +0300

    Phase0.2: retrieval gate + upload help + safer brief

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\besir\Desktop\syra_new"
+export "FLUTTER_APPLICATION_PATH=C:\Users\besir\.cursor\worktrees\syra_new\ymc"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.5"
-export "FLUTTER_BUILD_NUMBER=37"
+export "FLUTTER_BUILD_NUMBER=39"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
